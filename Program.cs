b81483bf228b8276aba78c2d0971340f074a914a
Ben Cofield <benrcofield@gmail.com>
2022-09-08 11:15:21 -0400

    Created new console app and added basic format

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace HelloWorld 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            //Wait for key press
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+            //Wait for key press
+            Console.WriteLine("Hello Again");
+            Console.ReadLine();
+        }
+    }
+}
